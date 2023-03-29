b9ac5713cc057518f02e0cca251e2cd43e28c2b2
JustWaveThings <justwavethings@proton.me>
2023-03-29 15:27:35 -0400

    Add test to make sure it would fail

--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -8,3 +8,12 @@ test('analyzeArray', () => {
 		length: 6,
 	});
 });
+
+test('analyzeArrayToFail', () => {
+	expect(analyzeArray([1, 8, 3, 4, 2, 6])).not.toEqual({
+		average: 5,
+		min: 2,
+		max: 6,
+		length: 3,
+	});
+});
